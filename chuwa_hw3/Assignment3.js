20230dd9b7b660d444d2938932afaca0195db1cc
Xinhui Shen <89504512+hayleexxhs@users.noreply.github.com>
2022-12-07 01:00:00 -0800

    202212070059 - Assignment3

--- chuwa_hw3/Assignment3.js
+++ chuwa_hw3/Assignment3.js
@@ -33,12 +33,62 @@ const tableInfo = {
   ],
 };
 
+const createTableHead = (head_name) => {
+  let th = document.createElement("th");
+  th.textContent = head_name;
+  return th;
+};
+
+const createTableContent = (s_name, age, phone, address) => {
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  td1.textContent = s_name;
+  let td2 = document.createElement("td");
+  td2.textContent = age;
+  let td3 = document.createElement("td");
+  td3.textContent = phone;
+  let td4 = document.createElement("td");
+  td4.textContent = address;
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  tr.appendChild(td4);
+  return tr;
+};
+
+const firstDiv = document.getElementById("firstDiv");
+let table = document.createElement("table");
+firstDiv.appendChild(table);
+let tr = document.createElement("tr");
+tableInfo.tableHeader.forEach((e) => {
+  tr.appendChild(createTableHead(e));
+});
+tableInfo.tableContent.forEach((e) => {
+  table.appendChild(
+    createTableContent(e["Student Name"], e.Age, e.Phone, e.Address)
+  );
+});
+
 /*
   Question 2
   Given the array and generate order list and unordered list dynamically as following:
   */
 
 const list = ["HTML", "JavaScript", "CSS", "React", "Redux", "Java"];
+
+const secondDiv = document.getElementById("secondDiv");
+const createlist = (isOrdered) => {
+  let listType = isOrdered ? "ol" : "ul";
+  let newList = document.createElement(listType);
+  list.forEach((e) => {
+    let li = document.createElement("li");
+    li.textContent = e;
+    newList.appendChild(li);
+  });
+  return newList;
+};
+secondDiv.appendChild(createlist(true));
+secondDiv.appendChild(createlist(false));
 
 /*
   Question 3
@@ -54,3 +104,13 @@ const dropDownList = [
   { value: "dalyCity", content: "Daly City" },
   { value: "sanJose", content: "San Jose" },
 ];
+
+const thirdDiv = document.getElementById("thirdDiv");
+let select = document.createElement("select");
+thirdDiv.appendChild(select);
+dropDownList.forEach(({ value, content }) => {
+  let opt = document.createElement("option");
+  opt.value = value;
+  opt.textContent = content;
+  select.appendChild(opt);
+});
