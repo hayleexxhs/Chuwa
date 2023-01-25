bd2d7671cdf07fd99a74affc39e3d026c28fbb1c
Xinhui Shen <89504512+hayleexxhs@users.noreply.github.com>
2023-01-25 01:09:06 -0800

    project-2 footer

--- projects/project-2/src/common/footer/index.jsx
+++ projects/project-2/src/common/footer/index.jsx
@@ -1,1 +1,35 @@
 import "./index.css";
+import { Col, Row, Space } from "antd";
+import {
+  YoutubeOutlined,
+  TwitterOutlined,
+  FacebookFilled,
+} from "@ant-design/icons";
+
+const Footer = () => {
+  return (
+    <div className="footer-div">
+      <Row align={"middle"}>
+        <Col span={6}>&copy; 2022 All Rights Reserved.</Col>
+        <Col span={12}>
+          <a href="" className="footer-icon">
+            <YoutubeOutlined />
+          </a>
+          <a href="" className="footer-icon">
+            <TwitterOutlined />
+          </a>
+          <a href="" className="footer-icon">
+            <FacebookFilled />
+          </a>
+        </Col>
+        <Col span={6}>
+          <a href="">Contact us</a>
+          <a href="">Privacy Policies</a>
+          <a href="">Help</a>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Footer;
