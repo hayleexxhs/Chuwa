0efa74f002be906db1200f769123f50446c666be
Xinhui Shen <89504512+hayleexxhs@users.noreply.github.com>
2023-01-25 00:56:56 -0800

    project-2 header

---
+++ projects/project-2/src/common/header/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./index.css";
+import { Col, Row, Input, Space } from "antd";
+import {
+  SearchOutlined,
+  UserOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
+
+const Header = () => {
+  const onSearch = (value) => console.log(value);
+  const suffix = (
+    <SearchOutlined
+      style={{
+        fontSize: 18,
+        color: "#979797",
+      }}
+    />
+  );
+
+  const handleSignIn = () => {
+    console.log("Onclick Sign In!");
+  };
+
+  return (
+    <>
+      <div>
+        <Row className="header-div" align={"middle"}>
+          <Col span={6} style={{ fontSize: "12px" }}>
+            <b>Management </b>Chuwa
+          </Col>
+          <Col span={8}>
+            <Input
+              className="header-input"
+              placeholder="Search"
+              onSearch={onSearch}
+              suffix={suffix}
+            />
+          </Col>
+          <Col span={10}>
+            <Space align="center" style={{ textAlign: "right" }}>
+              <span>
+                <UserOutlined style={{ fontSize: 28 }} />
+              </span>
+              <a onClick={handleSignIn}>Sign In</a>
+              <a>
+                <ShoppingCartOutlined style={{ fontSize: 28 }} />
+              </a>
+              <span>$0.00</span>
+            </Space>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+};
+
+export default Header;
